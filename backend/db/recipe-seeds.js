f584b1fe7de0efaa305814f32c34053fa1727135
sali1993 <salahudeenali28@gmail.com>
2021-06-19 18:43:13 -0400

    database seeded, some paths added

---
+++ backend/db/recipe-seeds.js
@@ -0,0 +1,20 @@
+const seedData = require("./recipe-seeds.json")
+const Recipe = require("../models/recipe-model")
+
+Recipe.deleteMany({})
+    .then(() => {
+        // Insert the dummy data and return it
+        // so we can log it in the next .then
+        return Recipe.insertMany(seedData);
+    })
+    // If the insert was successful, we'll see the
+    // results in the terminal
+    .then(console.log)
+    // Log the error if the insert didn't work
+    .catch(console.error)
+    // Whether it was successful or not, we need to 
+    // exit the database.
+    .finally(() => {
+        // Close the connection to Mongo
+        process.exit();
+    });
--- backend/models/recipe-model.js
+++ backend/models/recipe-model.js
@@ -2,30 +2,12 @@ const mongoose = require("../db/connection")
 
 const recipeSchema = new mongoose.Schema(
     {
-        title:{
-            type:String,
-            required: true,
-        },
-        cooktime:{
-            type:Number,
-            required: true,
-        },
-        vegetarian :{
-            type:Boolean,
-            required:true,
-        },
-        vegan :{
-            type:Boolean,
-            required:true,
-        },
-        ingredients :[{
-            type: String,
-            required: true,
-        }],
-        instructions : [{
-            type: String,
-            required: true,
-        }],       
+        title:String,
+        cooktime:Number,
+        vegetarian :Boolean,
+        vegan :Boolean,
+        ingredients :[],
+        instructions : []       
     }
 )
 
