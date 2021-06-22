622fab626d55087e4921efdbc381fac5ef1597b9
sali1993 <salahudeenali28@gmail.com>
2021-06-22 18:50:25 -0400

    updated routes

--- backend/controllers/cookbook.js
+++ backend/controllers/cookbook.js
@@ -2,10 +2,13 @@ const express = require("express")
 const router = express.Router()
 const Recipe = require("../models/recipe-model")
 
+//home route
 router.get("/",(req,res)=>{
     res.render("home")
 })
 
+
+//index route
 router.get("/recipes", (req, res) => {
 Recipe.find({})
         .then(recipes =>{
@@ -14,25 +17,45 @@ Recipe.find({})
         })
 })
 
+
+
+
+//new route
+router.get("/recipes/new",(req,res)=>{
+    res.render("new")
+})
+
+
+//create route
+router.post("/recipes",(req,res)=>{
+    console.log(req.body)
+    Recipe.create(req.body)
+            .then(res =>{
+                res.send(`${result}`)
+            })
+             .catch(err => {
+                 console.log(err)
+                 res.send("recipe not added")
+        })
+})
+
+
+
+//delete route
+
+
+
+//edit route
+
+//detail route
 router.get("/recipes/:id", (req, res) => {
     const routeId = req.params.id
     Recipe.findById(routeId)
-            .then(recipe =>{
-                res.render("detail", recipe)
-            })
-           
+        .then(recipe => {
+            res.render("detail", recipe)
+        })
+
 })
-
-router.get("/recipes/new",(req,res)=>{
-    res.render('new')
-})
-
-router.post("/recipes",(req,res)=>{
-    console.log(req.body)
-})
-
-
-
 
 
 module.exports = router
