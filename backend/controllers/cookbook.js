040d7c49abb14d13eb8cb9daa1f387fc68ffb0d6
sali1993 <salahudeenali28@gmail.com>
2021-06-27 14:37:55 -0400

    buggy user authentification

--- backend/controllers/cookbook.js
+++ backend/controllers/cookbook.js
@@ -1,38 +1,71 @@
 const express = require("express")
 const router = express.Router()
-const bcrypt = require("bcrypt")
+const passport = require("passport")
+const dotenv = require("dotenv")
+const util = require('util')
+const url = require('url')
+const querystring = require('querystring')
+
+dotenv.config()
+
 const Recipe = require("../models/recipe-model")
 
+//login
 
-const users=[]
-
-
-
-
-router.get("/users",(req,res)=>{
-    res.json(users)
+router.get('/login', passport.authenticate('auth0', {
+    scope: 'openid email profile'
+}), (req, res) => {
+    res.redirect('/landing');
 })
 
-router.post("/users", async (req,res)=>{
-    try {
-        const salt = await bcrypt.genSalt()
-        const hashedPassword = await bcrypt.hash(req.body.password, salt)
-        console.log(salt)
-        console.log(hashedPassword)
-        const user = { name: req.body.name, password: hashedPassword }
-        users.push(user)
-        res.status(201).send()
-    } catch {
-        res.status(500).send()
+router.get('/callback', (req, res, next) => {
+    passport.authenticate('auth0', function (err, user, info) {
+        if (err) { return next(err) }
+        if (!user) { return res.redirect('/login') }
+        req.logIn(user, function (err) {
+            if (err) { return next(err) }
+            const returnTo = req.session.returnTo
+            delete req.session.returnTo
+            res.redirect(returnTo || '/landing')
+        })
+    })(req, res, next)
+});
+
+
+//logout
+
+router.get('/logout', (req, res) => {
+    req.logout();
+
+    var returnTo = req.protocol + '://' + req.hostname;
+    var port = req.socket.localPort;
+    if (port !== undefined && port !== 80 && port !== 443) {
+        returnTo += ':' + port;
     }
-})
+    var logoutURL = new url.URL(
+        util.format('https://%s/v2/logout', process.env.ISSUER_BASE_URL)
+    );
+    var searchString = querystring.stringify({
+        client_id: process.env.CLIENT_ID,
+        returnTo: returnTo
+    });
+    logoutURL.search = searchString;
 
+    res.redirect(logoutURL);
+});
 
 
 
 //home route
+
 router.get("/",(req,res)=>{
     res.render("home")
+    // res.send(req.oidc.isAuthenticated()? "logged in": "logged out")
+})
+
+router.get("/landing", (req, res) => {
+    res.render("landing")
+  
 })
 
 
@@ -135,6 +168,7 @@ router.put("/recipes/:id/", (req, res) => {
         .catch(console.log)
 })
 
+
 //detail route
 router.get("/recipes/:id", (req, res) => {
     const routeId = req.params.id
@@ -144,6 +178,7 @@ router.get("/recipes/:id", (req, res) => {
         })
 
 })
+
 
 
 
--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,65 @@
 const express = require("express")
 const cookbookController= require("./controllers/cookbook")
 const methodOverride = require("method-override")
-const bcrypt = require("bcrypt")
-
 const app = express()
+const dotenv = require("dotenv")
+dotenv.config()
+const passport = require("passport")
+const Auth0Strategy = require('passport-auth0')
+const strategy = new Auth0Strategy(
+    {
+        domain: process.env.AUTH0_DOMAIN,
+        clientID: process.env.AUTH0_CLIENT_ID,
+        clientSecret: process.env.AUTH0_CLIENT_SECRET,
+        callbackURL:
+            process.env.AUTH0_CALLBACK_URL || 'http://localhost:1200/callback'
+    },
+    function (accessToken, refreshToken, extraParams, profile, done) {
+        return done(null, profile)
+    }
+)
+const session = require("express-session")
+const sess = {
+    secret: 'Hawks in 7',
+    cookie: {},
+    resave: false,
+    saveUninitialized: true
+}
+const userInViews = require('./controllers/middleware/userInViews');
+const authRouter = require('./controllers/auth');
+const usersRouter = require('./controllers/users');
+
+
+
+
+if (app.get('env') === 'production') {
+   sess.cookie.secure = true;
+    // app.set('trust proxy', 1);
+}
+
+
+passport.use(strategy);
+passport.serializeUser(function (user, done) {
+    done(null, user);
+});
+
+passport.deserializeUser(function (user, done) {
+    done(null, user);
+});
+
+
+app.use(passport.initialize())
+app.use(passport.session())
+app.use(session(sess))
+app.use(userInViews());
+app.use('/', authRouter)
+//authentication for the hbs
+app.use((req, res, next) => {
+    res.locals.isAutheticated = req.isAuthenticated()
+    next()
+})
+// app.use('/', usersRouter)
+
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "hbs")
