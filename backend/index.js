cca13233e7239cad9e34ee1f8669e58c27593a4b
sali1993 <salahudeenali28@gmail.com>
2021-06-29 13:53:51 -0400

    minor html edits

--- backend/index.js
+++ backend/index.js
@@ -9,11 +9,11 @@ const bodyParser = require("body-parser")
 const Auth0Strategy = require('passport-auth0')
 const strategy = new Auth0Strategy(
     {
-        domain: process.env.AUTH0_DOMAIN,
-        clientID: process.env.AUTH0_CLIENT_ID,
-        clientSecret: process.env.AUTH0_CLIENT_SECRET,
+        domain: process.env.domain,
+        clientID: process.env.clientID,
+        clientSecret: process.env.clientSecret,
         callbackURL:
-            process.env.AUTH0_CALLBACK_URL || 'http://localhost:1200/callback'
+            process.env.AUTH0_CALLBACK_URL || `http://${{domain}}/callback`
     },
     function (accessToken, refreshToken, extraParams, profile, done) {
         return done(null, profile)
